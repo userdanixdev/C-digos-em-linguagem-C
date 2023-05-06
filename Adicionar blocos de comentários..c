a81b04b87eaf06b47b9df00c3772e057d6ca1bc7
userdanixdev <132594952+userdanixdev@users.noreply.github.com>
2023-05-06 17:35:26 -0300

    Initial commit

---
+++ Adicionar blocos de comentários..c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+ 	/* Comando de comentário é barra virada
+ 	para a direita e asterisco. Com esse comando
+ 	vc cria um BLOCO de comentário
+ 	E NO FINAL, ASTERISCO e BARRA NO FINAL DO BLOCO
+ 	
+ 	*/
+ 	printf("HELLO WORLD");
+ 	
+ 	
+	return 0;
+}
