f89a73bbf951d48944abe3f69e169d7c8c1fad4f
userdanixdev <132594952+userdanixdev@users.noreply.github.com>
2023-05-07 23:15:39 -0300

    Create operador-size-of.c
    utilizando VETOR com o operador SIZEOF

---
+++ operador-size-of.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+ 	int vetor[10];
+   //operador sizeof//
+   //alocacao dinamica tipos primitivos ou de criacao//
+   //printf("Tamanho em memória de um tipo inteiro: %d bytes\n", sizeof(int));
+   //na arquitetura da minha maquina um inteiro sao 4 bytes//
+   //uma variavel inteiro possui 4 bytes//
+   //EXISTEM DUAS FORMAS://
+   
+   printf("Tamanho de um charactere na memoria: %d bytes\n", sizeof (vetor));
+// FLOAT tambem ocupa 32 bits na memoria//
+// para tipo primitivo usa paranteses no sizeof//
+// LEMBRANDO QUE E UM OPERADOR, CONSTANTE.nao e funcao//
+
+ 	
+ 	
+	return 0;
+}
