f776cace9e3d7fae6c9b80031f803ddb9301d4ce
userdanixdev <132594952+userdanixdev@users.noreply.github.com>
2023-05-08 11:33:54 -0300

    Create operadores-matematicos-simples-na-tela.c
    Conversão de tipo de variaveis com PARENTESES à ESQUERDA das VARIAVEIS

---
+++ operadores-matematicos-simples-na-tela.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+/* OPERADORES MATEMATICAS BASICAS:
+SOMA +
+SUBTRACAO - 
+MULTIPLICACAO *
+DIVISAO /
+*/
+/*
+printf("\nSoma: %d\n",10 +20);
+printf("\nSubtracao: %d\n", 10 - 20);
+ printf("\nMultiplicacao:  %d\n",10*20);
+	printf("\nDivisao:  %f\n", 10.0/20);
+//na divisao faz por numeros inteiros portanto mudar o divisor//
+	*/ 
+	//COM VARIAVEIS NA MATEMATICA//
+	//conversao de tipos de variaveis//
+int a = 10,b = 20;
+
+printf("\nSoma: %d\n",a +b);
+printf("\nSubtracao: %d\n", a - b);
+ printf("\nMultiplicacao:  %d\n",a*b);
+	printf("\nDivisao:  %.2f\n", (float)a/(float)b);
+//inteiro dividido por inteiro resultado INTEIRO//
+	
+	
+	return 0;
+}
