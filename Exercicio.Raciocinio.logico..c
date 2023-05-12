a95cdee79cbe82c8c36efb39c4222e3d6a5b480f
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-05-12 10:57:50 -0300

    IF.ELSE.aninhado+><
    Exercício usando função IF e ELSE aninhado com SINAIS MAIOR E MENOR.

---
+++ Exercicio.Raciocinio.logico..c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{	setlocale(LC_ALL,"Portuguese");
+
+/*Escreva um programa que leia três valores e apresente
+qual é o maior e qual é o maior
+*/
+ int a,b,c,menor,maior;
+ 
+ printf("Digite três valores:  ");
+ scanf("%d%d%d",&a,&b,&c);
+ 
+    // PRIMEIRA SOLUÇÃO//
+ if(a<b){
+ 
+ 	if(a<c)
+ 	menor = a;
+ 	else 
+ 		menor =c;
+}
+ else{
+ 
+ 	if(b<c)
+	 	menor =b;
+	else
+		menor =c;	 		
+  }
+	if(a>b){
+	
+		if(a>c)
+			maior=a;
+		else
+			maior =c;
+	}
+	else{
+	
+		if(b>c)
+			maior =b;
+		else
+			maior=c;
+						
+}
+		printf("Primeira versão\n Menor: %d\tMaior: %d\n",menor,maior);
+		
+	return 0;
+}
