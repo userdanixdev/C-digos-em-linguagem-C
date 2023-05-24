47e71118f01634ce00484a1d7449fa285d5ab526
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-05-23 23:32:27 -0300

    Add files via upload

---
+++ Estrutura de repetição.FOR.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+		
+		
+		//estrutura de repetição PARA //
+		
+int main()
+{
+
+int i =0;
+
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d",i++);
+printf("%d\n\n",i++);
+
+
+for(i=0;i<=20;i++)
+	printf("%d",i);
+// chaves opcionais //							
+
+
+
+	return 0;
+	
+	
+}
+
+
+
+
+
