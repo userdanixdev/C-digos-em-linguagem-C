85ea323e5ea83ce0fd02d50577d9d52a23cfb99c
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-05-23 23:42:31 -0300

    Add files via upload

---
+++ Estrutura de repetição.FOR.descrescente.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+		
+		
+		//estrutura de repetição PARA //
+		// incrimento descrescente //
+int main()
+{
+
+int i =10;
+
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d",i--);
+printf("%d\n\n",i--);
+
+
+for(i=10;i>=0;i--)
+	printf("%d",i);
+// chaves opcionais //							
+
+
+
+	return 0;
+	
+	
+}
+
+
+
+
+
