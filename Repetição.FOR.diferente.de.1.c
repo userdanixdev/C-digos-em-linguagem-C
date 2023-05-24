dfcf932c63e67ee5ab95bb194f572f08474738dd
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-05-24 12:57:06 -0300

    Add files via upload

---
+++ Repetição.FOR.diferente.de.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+		
+		
+		//estrutura de repetição PARA //
+		// encrimento diferente de 1 //
+int main()
+{
+
+int i;
+
+//for(i=1;i<=100;i+=2) // m,esma coisa que: i= i +2//
+//	if(i % 2 !=0) // não precisa do IF, portanto, pela troca de sinal que a variavel recebeu //
+		printf("%d", i);
+								
+// multiplos de cinco , por exemplo.
+for(i=5;i<=100;i+=5)
+printf("%d", i);
+
+	return 0;
+	
+	
+}
+
+
+
+
+
